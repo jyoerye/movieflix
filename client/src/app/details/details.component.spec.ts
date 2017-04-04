30d67458bd01c52c4fbeacacbe5abbbf6220fab1
Jyothsna Iyer <M1020536@mindtree.com>
2017-04-04 12:38:56 -0400

    Added mock components to detail spec

--- client/src/app/details/details.component.spec.ts
+++ client/src/app/details/details.component.spec.ts
@@ -3,38 +3,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-import {Router} from "@angular/router";
+import {Router,ActivatedRoute} from "@angular/router";
 import {MockRouter} from "../testing/MockRouter";
 import { DetailsComponent } from './details.component';
 import {DetailsService} from '../details.service';
 import {RatingComponent} from './rating.component';
 import { Component } from '@angular/core';  
 import { MockComponent } from 'ng2-mock-component';
+import {UserCommentsComponent} from '../usercomments/usercomments.component';
+import {ActivatedRouteStub} from '../testing/ActivatedRouteStub';
+
+
+@Component({selector: 'user-comments', template: ''})
+  class UserCommentsStubComponent {}
+@Component({selector: 'rating', template: ''})
+  class RatingStubComponent {}
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
-
-
-
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DetailsComponent,
-      MockComponent({ selector: 'rating' }),
-      MockComponent({ selector: 'user-comments' })
+      declarations: [ DetailsComponent,UserCommentsStubComponent,RatingStubComponent      
        ],
                      
       providers: [
                 { provide: Router, useClass: MockRouter},
-               { provide: DetailsService, useClass: MockDetailsService} ,                            
+               { provide: DetailsService, useClass: MockDetailsService} ,  
+                {provide: ActivatedRoute, useClass: ActivatedRouteStub}                          
             ]
     })
      
   //  .compileComponents();
      fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
-
+    activatedRoute = TestBed.get(ActivatedRouteStub)
 
   }));
 
