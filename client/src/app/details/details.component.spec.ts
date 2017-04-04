39cb8efc7833044d8550a355598955b2bce70521
Jyothsna Iyer <M1020536@mindtree.com>
2017-04-04 12:03:08 -0400

    Adding test cases for details component

--- client/src/app/details/details.component.spec.ts
+++ client/src/app/details/details.component.spec.ts
@@ -1,24 +1,33 @@
 //  tslint:disable:no-unused-variable 
 
-/*import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import {Router} from "@angular/router";
 import {MockRouter} from "../testing/MockRouter";
 import { DetailsComponent } from './details.component';
 import {DetailsService} from '../details.service';
+import {RatingComponent} from './rating.component';
+import { Component } from '@angular/core';  
+import { MockComponent } from 'ng2-mock-component';
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
 
+
+
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DetailsComponent ],
+      declarations: [ DetailsComponent,
+      MockComponent({ selector: 'rating' }),
+      MockComponent({ selector: 'user-comments' })
+       ],
+                     
       providers: [
-                { provide: Router, useClass: MockRouter}
-              //  { provide: DetailsService, useClass: MockDetailsService}       
-                               
+                { provide: Router, useClass: MockRouter},
+               { provide: DetailsService, useClass: MockDetailsService} ,                            
             ]
     })
      
@@ -42,4 +51,3 @@ class MockDetailsService{
 
 
 });
-*/
